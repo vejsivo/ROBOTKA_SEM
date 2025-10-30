886762b7ff951efdd3aa17af880d6bc53486a4b4
jurajpauko <jurajpauko04@gmail.com>
2025-10-30 12:26:54 +0100

    Cleaning.

--- move_robot.py
+++
@@ -1,9 +0,0 @@
-from ctu_crs import CRS93 # or CRS97
-
-robot = CRS93()  # set argument tty_dev=None if you are not connected to robot,
-# it will allow you to compute FK and IK offline
-robot.initialize()  # initialize connection to the robot, perform hard and soft home
-q = robot.get_q()  # get current joint configuration
-robot.move_to_q(q + [0.5, 0.0, 0.0, 0.0, 0.0, 0.0])  # move robot all values in radians
-robot.wait_for_motion_stop() # wait until the robot stops
-robot.close()  # close the connection
